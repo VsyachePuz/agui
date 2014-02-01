fac2f3c07cc188539c515a60186d21db1746561b
Brian Douglass <bhdouglass@gmail.com>
2014-02-01 14:16:41 -0500

    Added license to each file

--- agui/backends/pyside/extras/__init__.py
+++ agui/backends/pyside/extras/__init__.py
@@ -1,5 +1,21 @@
 from agui.backends.pyside.extras.about import About
-from agui.backends.pyside.extras.icon import Icon
+f# -*- Mode: Python; coding: utf-8; indent-tabs-mode: nil; tab-width: 4 -*-
+### BEGIN LICENSE
+# Copyright (C) 2014 Brian Douglass bhdouglass@gmail.com
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranties of
+# MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+### END LICENSE
+
+rom agui.backends.pyside.extras.icon import Icon
 from agui.backends.pyside.extras.message import Message
 from agui.backends.pyside.extras.popup import Popup
 from agui.backends.pyside.extras.sound import Sound
